d1e4f63d798388e3d675d2375c4a8604adaa02c6
ursisterbtw <ursisterbtw@gmail.com>
2025-06-27 07:39:09 -0400

    chore: fix formatting in benchmark file

--- benches/markdown_bench.rs
+++ benches/markdown_bench.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use criterion::{BenchmarkId, Criterion, black_box, criterion_group, criterion_main};
 use markdown_lab_rs::{
     chunker::create_semantic_chunks,
     html_parser::{clean_html, extract_links, extract_main_content},
